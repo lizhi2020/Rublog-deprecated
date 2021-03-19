cd932ac7e03dd16ea55eb539cbdc6c56993cec2d
lizhi <lizhiwj2019@outlook.com>
2021-03-19 21:51:16 +0800

    v0.1.0 finished

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,42 @@
-/*
-use std::fs;
-use std::net::{TcpListener,TcpStream};
-use std::path::{PathBuf,Path};
-use std::env;
+use tera::{Tera,Context};
 use comrak::{markdown_to_html, ComrakOptions};
-use json::parse;
-use json::JsonValue;
-use std::io::{Read,Write};
+use structopt::StructOpt;
 
-fn default_config()-> JsonValue{
-
-    let seq: String=String::from(r#"
-{
-    "author":"Unknow"
+#[derive(StructOpt,Debug)]
+#[structopt(about = "the stupid content tracker")]
+enum Rub {
+    Init {
+        name:String
+    },
+    New {
+        pattern:String,
+        name:String
+    },
+    Clear {
+        name:Option<String>
+    },
+    Build
 }
 
-    "#);
-
-    let p=PathBuf::from("config.json");
-    fs::write(p, seq.as_str()).expect("默认配置创建失败");
-
-    let config=parse(seq.as_str()).expect("返回json对象失败");
-    return config;
-}
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    println!("{:?}",buffer);
-    stream.read(&mut buffer).unwrap();
-    let get = b"GET / HTTP/1.1\r\n";
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "main.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
-
-    //let contents = fs::read_to_string(filename).expect(filename);
-    let contents = String::from("<html>hello</html>");
-    let response = format!("{}{}", status_line, contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-    
-    //let ten_millis = time::Duration::from_millis(10000); 
-    //thread::sleep(ten_millis);				//睡眠一段时间，模拟处理时间很长
-}
-fn server()->std::io::Result<()>{
-    let listener=TcpListener::bind("127.0.0.1:8080")?;
-    for stream in listener.incoming(){
-        handle_client(stream?);
+fn clear(name:&Option<String>){
+    let mut path=env::current_dir().unwrap();
+    if let Some(name)=name{
+        path.push(name);
     }
-    Ok(())
-}
-fn build(tera:& tera::Tera){
-    let curdir=env::current_dir().unwrap();
-    println!("curdir: {}", curdir.display());
-    println!("curexe: {}", env::current_exe().unwrap().display());
 
-    //读取配置文件
-    let config=PathBuf::from("config.json");
-    let config=fs::read_to_string(config);
-    let config_json: JsonValue;
-    match config {
-        Ok(config)=>{
-            println!("找到配置文件");
-            config_json=parse(config.as_str()).unwrap();
-        },
-        _=>{
-            config_json=default_config();
-        }
-    };
-
-    let config = config_json;
-    println!("{}",config.pretty(4));
-
-    let target_path=PathBuf::from("target");
-    if target_path.is_dir(){
-        println!("目标文件夹已存在，这可能导致问题");
+    for entry in ["src","templates","build"].iter(){
+        path.push(entry);
+        fs::remove_dir_all(&path).unwrap_or_else(|e|{
+            println!("{}{:?}",e,path);
+        });
+        path.pop();
     }
-    else{
-        //然后试图创建输出文件夹./build
-        fs::create_dir(PathBuf::from("build")).expect("目标文件夹创建失败，程序终止");
-    }
-    
-    //遍历相对路径./source下文本文件内容,并输出到target下
 
-
-    let source_dir = PathBuf::from("source");
-
-    for entry in source_dir.read_dir().expect("read error"){
-        if let Ok(entry)=entry{
-            if entry.path().is_file() {
-                println!("{:?}",entry.path());
-                let f = fs::read_to_string(entry.path()).unwrap();
-                let mut p=PathBuf::from("build/blog");
-                p.push(entry.path().file_name().unwrap());
-                p.set_extension("html");
-                println!("{:?}",p);
-
-                let output=markdown_to_html(f.as_str(), &ComrakOptions::default());
-                println!("{}",output);
-                
-                let mut context=Context::new();
-                context.insert("post_context", &output);
-
-                let content=tera.render("post.html", &context).unwrap();
-
-                
-                fs::write(p, content).expect(entry.path().to_str().unwrap());
-            };
-        }
-    };
 }
-
-use tera::Tera;
-use tera::Context;
-
-*/
-fn main() {
-
-    println!("hello world");
-   /*
-    let tera = match Tera::new("templates/**/*.html") {
+//渲染
+fn build()->std::io::Result<()>{
+    //初始化 tera
+    let mut tera = match Tera::new("templates/*.html") {
         Ok(t) => t,
         Err(e) => {
             println!("Parsing error(s): {}", e);
@@ -128,48 +44,131 @@ fn main() {
         }
     };
 
-    let mut context = Context::new();
-    context.insert("title","hello world");
-    context.insert("home_page_title", "少年东方曜");
-    context.insert("about_url", "about.html");
-    context.insert("blog_url", "blog/index.html");
-    //context.insert("vat_rate", &0.20);
-    //context.insert("users");
-    //println!("{}",tera.render("home.html", &context).unwrap());
+    tera.autoescape_on(vec![]);
+    //只对md文件渲染
+    for entry in fs::read_dir(PathBuf::from("src")).unwrap(){
 
-    //生成首页
-    let content=tera.render("home.html",&context).unwrap();
-    fs::write(PathBuf::from("build/home.html"), content).unwrap();
-
-    
-    //生成about
-    context.insert("title", "About");
-    context.insert("about_context", "爱好技术，喜欢交流");
-    context.insert("author_name", "li");
-    let content=tera.render("about.html",&context).unwrap();
-    fs::write(PathBuf::from("build/about.html"), content).unwrap();
-
-    //生成blog/
-    build(& tera);
- 
-    let mut arg=env::args();
-    match arg.nth(1){
-        Some(p)=>{
-            if p== String::from("server"){
-                println!("start server");
-                server().unwrap();
-            }
-            else if p== String::from("build"){
-                println!("start build");
-                build();
-            }
-            else{
-                println!("unknow command");
-            }
+        let entry=entry?;
+        let path=entry.path();
+        if path.extension().unwrap() != "md"{
+            continue;
         }
-        None=>{
-            println!("no command!");
+        let mut dst=PathBuf::from("build");
+        dst.push(path.file_name().unwrap());
+        dst.set_extension("html");
+        render(&tera,&path,&dst).unwrap();
+    };
+    Ok(())
+}
+//上下文,源文件,目标文件
+fn render(tera:&Tera,src:&PathBuf,dst:&PathBuf)->std::io::Result<()>{
+    let src=fs::read_to_string(src)?;
+    let post_context=markdown_to_html(&src, &ComrakOptions::default());
+
+    //根据源文件生成上下文
+    let mut context=Context::new();
+
+    context.insert("post_context", &post_context);
+    let content=tera.render("post.html", &context).unwrap();
+
+    fs::write(dst, content).unwrap();
+    Ok(())
+}
+fn new(pattern:String,name:String){
+    if pattern==String::from("post"){
+        let mut path=PathBuf::from("src");
+        path.push(name);
+
+        path.set_extension("md");
+        fs::write(path, "").unwrap();
+    }
+    else{
+        panic!("unkonw pattern");
+    }
+}
+fn main() {
+
+    let args= Rub::from_args();
+    match args{
+        Rub::Init{name}=>{
+            init_project(&name);
+        }
+        Rub::New{pattern,name}=>{
+            new(pattern,name);
+        }
+        Rub::Clear{name}=>{
+            clear(&name);
+        }
+        Rub::Build=>{
+            build().unwrap();
         }
     }
-*/
+}
+use std::{fs,path::PathBuf};
+use std::io::ErrorKind;
+use std::env;
+
+///创建项目目录
+fn init_project(path:&String){
+    let mut path=PathBuf::from(path);
+
+    //先尝试生成项目根目录
+    match fs::create_dir(&path){
+        Ok(())=>{
+            println!("create dir:{:?}",path)
+        }
+        Err(error)=>{
+            if error.kind()==ErrorKind::AlreadyExists{
+                println!("dir:{:?} has existed",path)
+            }
+            else{
+                panic!("unknow error happened when creating dir:{:?}",path)
+            }
+        }
+    };
+
+    // 创建子目录
+    for i in ["src","build","templates"].iter(){
+        path.push(i);
+        match fs::create_dir(&path){
+            Ok(())=>{
+                println!("create dir:{:?}",path)
+            }
+            Err(error)=>{
+                if error.kind()==ErrorKind::AlreadyExists{
+                    println!("warning dir:{:?} has exited",path)
+                }
+                else{
+                    panic!("unknow error happened when creating dir:{:?}",path)
+                }
+            }
+        };
+        path.pop();
+    }
+
+    //执行拷贝templates 操作
+    //将exe路径下的template 拷贝到 项目中
+
+    let mut template_dir=env::current_exe().unwrap();
+    template_dir.push("templates");
+
+    if !template_dir.is_dir(){
+        template_dir=env::current_dir().unwrap();
+        template_dir.push("templates");
+    }
+
+    for entry in template_dir.read_dir().expect(template_dir.to_str().unwrap()){
+        if let Ok(entry)=entry{
+            if entry.path().is_file() {
+                let f = fs::read_to_string(entry.path()).unwrap();
+                println!("read file:{:?}",entry.path());
+
+                let mut p=PathBuf::from(&path);
+                p.push("templates");
+                p.push(entry.path().file_name().unwrap());
+                fs::write(&p, f).expect(entry.path().to_str().unwrap());
+                println!("create file:{:?}",p);
+            };
+        }
+    }
 }
